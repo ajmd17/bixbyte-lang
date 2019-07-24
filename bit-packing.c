3258dccd186d5d53ccf09746cbf6d541aae800e2
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2019-07-24 16:08:54 -0300

    Update bit-packing.c

--- bit-packing.c
+++ bit-packing.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+// ===== Instructions =====
+
 enum ARCHETYPE {
   AT_DATA = 0x1,
   AT_LOCAL = 0x2,
@@ -9,9 +11,9 @@ enum ARCHETYPE {
   AT_ABS = 0xC
 };
 
-typedef int32_t obj_loc_t;
+typedef uint32_t obj_loc_t;
 typedef uint8_t archtype_t;
-typedef int32_t loc_28_t;
+typedef uint32_t loc_28_t;
 
 obj_loc_t obj_loc_make(loc_28_t loc, archtype_t at) {
   obj_loc_t payload = loc;
@@ -24,6 +26,8 @@ void obj_loc_parse(obj_loc_t payload, loc_28_t *loc, archtype_t *at) {
   *loc = payload >> 4;
   *at = payload & 0xF;
 }
+
+// ===== Values =====
 
 typedef uint32_t metadata_t;
 
@@ -51,31 +55,53 @@ VALUE_TYPE value_getType(value_t *value) {
   return (VALUE_TYPE)(value->metadata & 0xFF);
 }
 
-typedef struct {
-  value_t *storage[4];
-} datatable_t;
+// ===== Data storage =====
 
 #define MB_TO_BYTES(mb) (mb*1000000)
-
-#define DEFAULT_STATIC_DATA_SIZE 150
-#define DEFAULT_STACK_SIZE 256 // in MB
-#define STACK_SIZE_BYTES (MB_TO_BYTES(DEFAULT_STACK_SIZE) - (MB_TO_BYTES(DEFAULT_STACK_SIZE) % sizeof(value_t)))
+#define DEFAULT_STATIC_DATA_SIZE_MB 150 // in MB
+#define STATIC_DATA_SIZE_BYTES (MB_TO_BYTES(DEFAULT_STATIC_DATA_SIZE_MB) - (MB_TO_BYTES(DEFAULT_STATIC_DATA_SIZE_MB) % sizeof(value_t)))
+#define DEFAULT_STACK_SIZE_MB 256 // in MB
+#define STACK_SIZE_BYTES (MB_TO_BYTES(DEFAULT_STACK_SIZE_MB) - (MB_TO_BYTES(DEFAULT_STACK_SIZE_MB) % sizeof(value_t)))
 #define NUM_REGISTERS 4
 
+typedef struct {
+  value_t *data;
+  size_t len;
+} storage_t;
+
+typedef struct {
+  storage_t storage[4];
+} datatable_t;
+
 void datatable_init(datatable_t *dt) {
-  dt->storage[0] = NULL;
+  dt->storage[0].data = NULL;
+  dt->storage[0].len = 0;
 
-  dt->storage[AT_DATA] = malloc(sizeof(value_t) * DEFAULT_STATIC_DATA_SIZE);
-  dt->storage[AT_LOCAL] = malloc(STACK_SIZE_BYTES);
-  dt->storage[AT_REG] = malloc(sizeof(value_t) * NUM_REGISTERS);
+  dt->storage[AT_DATA].data = malloc(STATIC_DATA_SIZE_BYTES);
+  memset(dt->storage[AT_DATA].data, 0, STATIC_DATA_SIZE_BYTES);
+  dt->storage[AT_DATA].len = 0;
 
-  memset(dt->storage[AT_DATA], 0, sizeof(value_t) * DEFAULT_STATIC_DATA_SIZE);
-  memset(dt->storage[AT_LOCAL], 0, STACK_SIZE_BYTES);
-  memset(dt->storage[AT_REG], 0, sizeof(value_t) * NUM_REGISTERS);
+  dt->storage[AT_LOCAL].data = malloc(STACK_SIZE_BYTES);
+  memset(dt->storage[AT_LOCAL].data, 0, STACK_SIZE_BYTES);
+  dt->storage[AT_LOCAL].len = 0;
+
+  dt->storage[AT_REG].data = malloc(sizeof(value_t) * NUM_REGISTERS);
+  memset(dt->storage[AT_REG].data, 0, sizeof(value_t) * NUM_REGISTERS);
+  dt->storage[AT_REG].len = 0;
 }
 
+value_t *datatable_getValue(datatable_t *dt, loc_28_t loc, archtype_t at) {
+  storage_t *s = &dt->storage[at & 0x3];
 
-// =====
+  int abs = ((at & 0xC) >> 2) - 2;
+  size_t idx = (-abs & loc) | ((abs - 1) & (s->len - loc));
+
+  return s->data[idx];
+}
+
+// ===== VM =====
+
+// ===== Main driver =====
 int main() {
   obj_loc_t o = obj_loc_make(123/* 28-bit int */, AT_DATA | AT_ABS);
 
