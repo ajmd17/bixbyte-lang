f564b919ebf2c5ccf40253e4767611b4d96a9e6c
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2019-07-25 10:19:51 -0300

    Create bound_variables.hpp

---
+++ include/bcparse/bound_variables.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <memory>
+
+#include <bcparse/ast/ast_expression.hpp>
+
+template <typename T>
+using Pointer = std::shared_ptr<T>;
+
+namespace bcparse {
+  class BoundVariables {
+  public:
+    BoundVariables();
+    BoundVariables(const BoundVariables &other);
+
+    Pointer<AstExpression> &get(const std::string &name);
+    const Pointer<AstExpression> &get(const std::string &name) const;
+    void set(const std::string &name, const Pointer<AstExpression> &value);
+
+  private:
+    std::map<std::string, Pointer<AstExpression>> m_map;
+  };
+}
