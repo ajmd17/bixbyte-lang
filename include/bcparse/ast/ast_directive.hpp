8c077bb6510b95a0c0555a1686ed0c707d816ad2
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2019-07-25 10:44:26 -0300

    Update ast_directive.hpp

--- include/bcparse/ast/ast_directive.hpp
+++ include/bcparse/ast/ast_directive.hpp
@@ -11,6 +11,34 @@ template <typename T>
 using Pointer = std::shared_ptr<T>;
 
 namespace bcparse {
+  class AstDirectiveImpl {
+    friend class AstDirective;
+  protected:
+    AstDirectiveImpl(const std::vector<Pointer<AstExpression>> &arguments,
+      const std::string &body,
+      const SourceLocation &location);
+    AstDirectiveImpl(const AstDirectiveImpl &other) = delete;
+    virtual ~AstDirectiveImpl() = default;
+    
+    virtual void visit(AstVisitor *visitor, Module *mod) = 0;
+    virtual void build(AstVisitor *visitor, Module *mod, BytecodeChunk *out) = 0;
+    virtual void optimize(AstVisitor *visitor, Module *mod) = 0;
+
+    std::vector<Pointer<AstExpression>> m_arguments;
+    std::string m_body;
+  };
+  
+  class AstMacroDirective : public AstDirectiveImpl {
+  private:
+    AstMacroDirective(const std::vector<Pointer<AstExpression>> &arguments,
+      const std::string &body,
+      const SourceLocation &location);
+
+    virtual void visit(AstVisitor *visitor, Module *mod) override;
+    virtual void build(AstVisitor *visitor, Module *mod, BytecodeChunk *out) override;
+    virtual void optimize(AstVisitor *visitor, Module *mod) override;
+  };
+  
   class AstDirective : public AstStatement {
   public:
     AstDirective(const std::string &name,
@@ -28,6 +56,8 @@ namespace bcparse {
     std::string m_name;
     std::vector<Pointer<AstExpression>> m_arguments;
     std::string m_body;
+
+    AstDirectiveImpl *m_impl;
 
     inline Pointer<AstDirective> CloneImpl() const {
       return Pointer<AstDirective>(new AstDirective(
